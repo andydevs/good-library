8c04c5bc900d4c97a6bcd15f40539224c51d9e42
andydev <akanshul97@gmail.com>
2017-10-26 13:10:57 -0400

    Updated header in files

--- good/annotation.py
+++ good/annotation.py
@@ -1,7 +1,8 @@
 """
 The Good Library
 
-A collection of programming and syntax tools for experienced Python users
+The Good Library is a collection of programming and syntax tools that makes your
+python code more expressive and easier to work with.
 
 Author:  Anshul Kharbanda
 Created: 10 - 6 - 2017
