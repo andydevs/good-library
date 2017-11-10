707f54ddd5d45c7e9487d88c9cbf10b3f1619bdf
andydev <akanshul97@gmail.com>
2017-11-10 00:30:04 -0500

    Moved handler tests to handlers_test folder

--- test/__init__.py
+++ test/__init__.py
@@ -6,8 +6,8 @@ A collection of programming and syntax tools for experienced Python users
 Author:  Anshul Kharbanda
 Created: 10 - 6 - 2017
 """
+from .handlers_test.init_test import NameInitHandlerTest, UnderscoreInitHandlerTest, DunderInitHandlerTest
+from .handlers_test.string_test import ValueStringHandlerTest, KeyValueStringHandlerTest
 from .access_test import GetTest, SetTest, GetSetTest, ConstTest
 from .annotation_test import AnnotationTypeTest, CreateConstructorTest, AnnotationConstructorTest, GetAllTest
-from .handlers_init_test import NameInitHandlerTest, UnderscoreInitHandlerTest, DunderInitHandlerTest
-from .handlers_string_test import ValueStringHandlerTest, KeyValueStringHandlerTest
 from .interface_test import ISpecTest, InterfaceTest, ImplementsTest
